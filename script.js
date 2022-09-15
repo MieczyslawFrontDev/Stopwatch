24998eccd4574b39ad7058b61b97bbd8f35d4aff
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-09-16 01:58:53 +0200

    get buttons elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const startBtn = document.querySelector(".start");
+const pauseBtn = document.querySelector(".pause");
+const stopBtn = document.querySelector(".stop");
+const resetBtn = document.querySelector(".reset");
