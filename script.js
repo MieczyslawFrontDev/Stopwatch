24c6faa67c330c20d9a0bbb0f5173b0645bcb413
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-09-16 02:00:23 +0200

    created countTime, minutes and seconds variables

--- script.js
+++ script.js
@@ -3,3 +3,7 @@ const pauseBtn = document.querySelector(".pause");
 const stopBtn = document.querySelector(".stop");
 const resetBtn = document.querySelector(".reset");
 const historyBtn = document.querySelector(".history");
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
