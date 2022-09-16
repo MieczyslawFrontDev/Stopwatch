16d804301687583f76a42c65f00fe8b3d7ae75c6
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-09-16 02:09:19 +0200

    get time el, func handlePause and handleStop created, set eventListener on pauseBtn and stopBtn

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const stopBtn = document.querySelector(".stop");
 const resetBtn = document.querySelector(".reset");
 const historyBtn = document.querySelector(".history");
 const stopwatch = document.querySelector(".stopwatch");
+const time = document.querySelector(".time");
 
 let countTime;
 let minutes = 0;
@@ -25,4 +26,23 @@ const handleStart = () => {
   }, 100);
 };
 
+const handlePause = () => {
+  clearInterval(countTime);
+};
+
+const handleStop = () => {
+  clearInterval(countTime);
+  if (stopwatch.textContent !== "0:00") {
+    time.style.visibility = "visible";
+  }
+  time.textContent = `Ostatni czas: ${stopwatch.textContent}`;
+
+  stopwatch.textContent = "0:00";
+  timeList.textContent = "";
+  seconds = 0;
+  minutes = 0;
+};
+
 startBtn.addEventListener("click", handleStart);
+pauseBtn.addEventListener("click", handlePause);
+stopBtn.addEventListener("click", handleStop);
