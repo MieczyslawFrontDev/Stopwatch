b9a799839ed2bd285b4665ee9296f61f0036d4a1
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-09-16 02:04:35 +0200

    get stopwatch el, handleStart func created and set the eventListener on startBtn el

--- script.js
+++ script.js
@@ -3,7 +3,26 @@ const pauseBtn = document.querySelector(".pause");
 const stopBtn = document.querySelector(".stop");
 const resetBtn = document.querySelector(".reset");
 const historyBtn = document.querySelector(".history");
+const stopwatch = document.querySelector(".stopwatch");
 
 let countTime;
 let minutes = 0;
 let seconds = 0;
+
+const handleStart = () => {
+  clearInterval(countTime);
+  countTime = setInterval(() => {
+    seconds++;
+    if (seconds < 9) {
+      stopwatch.textContent = `${minutes}:0${seconds}`;
+    } else if (seconds >= 9 && seconds < 59) {
+      stopwatch.textContent = `${minutes}:${seconds}`;
+    } else {
+      minutes++;
+      seconds = 0;
+      stopwatch.textContent = `${minutes}:00`;
+    }
+  }, 100);
+};
+
+startBtn.addEventListener("click", handleStart);
